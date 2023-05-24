9b2cd00f07b9716dd5a11d53dec85c0e4e58fdfd
theeduke <oscarian528@gmail.com>
2023-05-24 14:07:35 -0700

    task 1

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/ env python3
+"""
+simple flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('0-index.html')
+
+if __name__ = "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
